38569375d9f5f2c8150318b5213d7bf664edf646
akabetta <omerr_ylmz@hotmail.com>
2023-12-03 16:57:28 +0300

    index page and search funct ok

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask
-from flask import render_template,request
+from flask import render_template,request,redirect,url_for
 
 COUNTRY_LIST = []
 countries_requests = requests.get("https://restcountries.com/v3.1/all")
@@ -15,7 +15,6 @@ for countries in countries_data:
   country = countries["name"]["common"].lower()
   COUNTRY_LIST.append(country)
 
-print(len(COUNTRY_LIST))
 country_language = list(country_data[0]["languages"].keys())
 if country_language:
   country_language = country_language[0]
@@ -32,8 +31,6 @@ country_common_translate = country_data[0]["translations"][
 country_official_translate = country_data[0]["translations"][
     f"{tranlastion_lang}"]["official"]
 
-print(country_common_translate, "--", country_official_translate)
-print(country_common, "--", country_official)
 
 
 app=Flask(__name__)
@@ -49,10 +46,11 @@ def search():
     user_input = request.form['country'].lower()
     if user_input in COUNTRY_LIST:
         result = user_input.capitalize()
+        return render_template('index.html', result=result)
     else:
-        result = None
-
-    return render_template('index.html', result=result)
+        result=None
+        return render_template("index.html", countries=[country.capitalize() for country in COUNTRY_LIST],user_input=user_input,result=result)
+    
 
 
 if __name__ == "__main__":
