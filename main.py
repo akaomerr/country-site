fc49c8a197ddd5c78381f5e3b9791d1ef31edf3f
akabetta <omerr_ylmz@hotmail.com>
2023-12-09 20:05:47 +0300

    country page starting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request
 
 COUNTRY_LIST = []
 countries_requests = requests.get("https://restcountries.com/v3.1/all")
@@ -15,42 +15,48 @@ for countries in countries_data:
   country = countries["name"]["common"].lower()
   COUNTRY_LIST.append(country)
 
-country_language = list(country_data[0]["languages"].keys())
-if country_language:
-  country_language = country_language[0]
-
-tranlastion_lang = "tur"
-
-country_common = country_data[0]["name"]["nativeName"][f"{country_language}"][
-    "common"]
-country_official = country_data[0]["name"]["nativeName"][
-    f"{country_language}"]["official"]
-
-country_common_translate = country_data[0]["translations"][
-    f"{tranlastion_lang}"]["common"]
-country_official_translate = country_data[0]["translations"][
-    f"{tranlastion_lang}"]["official"]
-
-
-
 app=Flask(__name__)
 
 
 @app.route("/")
 def index():
-  return render_template("index.html", countries=[country.capitalize() for country in COUNTRY_LIST])
+  return render_template("index.html", countries=[country.title() for country in COUNTRY_LIST])
 
 
 @app.route('/search',methods=['POST'])
 def search():
     user_input = request.form['country'].lower()
     if user_input in COUNTRY_LIST:
-        result = user_input.capitalize()
+        result = user_input.title()
         return render_template('index.html', result=result)
     else:
         result=None
-        return render_template("index.html", countries=[country.capitalize() for country in COUNTRY_LIST],user_input=user_input,result=result)
+        return render_template("index.html", countries=[country.title() for country in COUNTRY_LIST],user_input=user_input,result=result)
+
+@app.route('/<country>')
+def country(country):
+    country_url=f"https://restcountries.com/v3.1/name/{country}"
+    c=requests.get(country_url)
+    c_data=c.json()
+    for cs in c_data:
+       official=cs["name"]["official"]
+       flag=cs["flags"]["png"]
+       location=cs["maps"]["googleMaps"]
+       lat_lng=cs["latlng"]#list
+       capital=cs["capital"]
+       subregion=cs["subregion"]
+       languages=cs["languages"]
+       for currencies in cs["currencies"]:
+          currencie=currencies
+          currencie_symbol=cs["currencies"][currencie]["symbol"]
+       area=cs["area"]
+       population=cs["population"]
+
     
+    
+    country_description = "deneme"
+    return render_template('country.html', country=country, description=country_description, official=official,flag=flag,location=location,lat_lng=lat_lng,capital=capital,
+                           subregion=subregion, languages=languages, currencie=currencie, currencie_symbol=currencie_symbol, area=area,population=population)
 
 
 if __name__ == "__main__":
